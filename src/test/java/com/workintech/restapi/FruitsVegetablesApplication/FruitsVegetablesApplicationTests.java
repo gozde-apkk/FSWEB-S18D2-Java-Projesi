ae126a40c881a5cdbe5458eb90370acb71979961
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-09-04 21:21:30 +0300

    Spring-boot added

---
+++ src/test/java/com/workintech/restapi/FruitsVegetablesApplication/FruitsVegetablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.restapi.FruitsVegetablesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsVegetablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
